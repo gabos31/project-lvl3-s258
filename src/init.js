51b461b4f592681f54996df2c70428d23526b272
gabos31 <exbt@yandex.ru>
2018-06-14 15:44:15 +0200

    part 3

--- src/init.js
+++ src/init.js
@@ -4,19 +4,16 @@ import isURL from 'validator/lib/isURL';
 import $ from 'jquery';
 
 export default () => {
-  const state = {};
-  state.feeds = {};
-  state.articlesList = [];
+  const state = {
+    checkUrlResult: 'empty',
+    feeds: {},
+    articlesList: [],
+  };
   const proxy = 'https://cors-anywhere.herokuapp.com/';
   const inputFeed = document.getElementById('inputFeed');
   const inputForm = document.getElementById('inputForm');
   const mainAlert = document.getElementById('mainAlert');
   const submitBtn = document.getElementById('submitBtn');
-
-  const timeout = time => setTimeout(() => {
-    mainAlert.className = '';
-    mainAlert.textContent = '';
-  }, time);
 
   const activateForm = () => {
     inputFeed.removeAttribute('readonly');
@@ -31,56 +28,99 @@ export default () => {
       mainAlert.classList.replace('alert-primary', 'alert-success');
       mainAlert.textContent = 'RSS-Feed and articles added!';
     }
-    timeout(2000);
+    setTimeout(() => {
+      mainAlert.className = '';
+      mainAlert.textContent = '';
+    }, 2000);
     return data;
   };
 
   const checkUrl = (url) => {
     if (url === '') {
-      state.checkUrlResult = null;
+      state.checkUrlResult = 'empty';
+    } else if (isURL(url) && !_.has(state.feeds, url)) {
+      state.checkUrlResult = 'valid';
     } else {
-      state.checkUrlResult = (isURL(url) && !_.has(state.feeds, url));
+      state.checkUrlResult = 'invalid';
     }
   };
 
-  const parseRss = (data, url) => {
+  const parseRss = (data) => {
     const parser = new DOMParser();
     const parsedRss = parser.parseFromString(data, 'application/xml');
     if (parsedRss.doctype !== null) {
       throw new Error('This page does not contain rss.');
     }
+    return parsedRss;
+  };
+
+  const extractRssElements = (parsedRss) => {
+    const feedTitleElm = parsedRss.querySelector('channel > title');
+    const feedDescriptionElm = parsedRss.querySelector('channel > description');
+    const articleLinksColl = parsedRss.querySelectorAll('item > link');
+    const articleTitlesColl = parsedRss.querySelectorAll('item > title');
+    const articleDescriptionsColl = parsedRss.querySelectorAll('item > description');
+    return {
+      feedTitleElm,
+      feedDescriptionElm,
+      articleLinksColl,
+      articleTitlesColl,
+      articleDescriptionsColl,
+    };
+  };
+
+  const extractRssData = ({
+    feedTitleElm,
+    feedDescriptionElm,
+    articleLinksColl,
+    articleTitlesColl,
+    articleDescriptionsColl,
+  }) => {
+    const feedTitle = feedTitleElm.textContent;
+    const feedDescription = feedDescriptionElm.textContent;
+    const articleLinks = [];
+    const articleTitles = [];
+    const articleDescriptions = [];
+    articleLinksColl.forEach(link => articleLinks.push(link.textContent));
+    articleTitlesColl.forEach((title, i) => articleTitles.push(title.textContent ||
+      articleDescriptionsColl.item(i).textContent.split('<')[0]));
+    articleDescriptionsColl.forEach(description =>
+      articleDescriptions.push(description === null || description.textContent[0] === '<' ?
+        'This article does not have a description' : description.textContent.split('<')[0]));
+    return {
+      feedTitle,
+      feedDescription,
+      articleLinks,
+      articleTitles,
+      articleDescriptions,
+    };
+  };
+
+  const saveRss = (parsedRss, url) => {
     state.feeds[url] = parsedRss;
   };
 
   const makeFeedList = (url) => {
-    const doc = state.feeds[url];
-    const title = doc.querySelector('channel > title').textContent;
-    const description = doc.querySelector('channel > description').textContent;
+    const { feedTitle, feedDescription } = state.feeds[url];
     const feedTable = document.getElementById('feedTable');
     const tr = feedTable.insertRow();
     const tdTitle = tr.insertCell();
-    tdTitle.textContent = title;
+    tdTitle.textContent = feedTitle;
     const tdDescription = tr.insertCell();
-    tdDescription.textContent = description;
+    tdDescription.textContent = feedDescription;
     inputFeed.value = '';
     inputFeed.classList.remove('is-valid');
   };
 
   const makeArticlesList = (url) => {
-    const doc = state.feeds[url];
+    const { articleLinks, articleTitles, articleDescriptions } = state.feeds[url];
     const articlesUl = document.getElementById('articlesList');
-    const itemsColl = doc.querySelectorAll('item');
-    itemsColl.forEach(item => state.articlesList.push(item));
-    state.articlesList.forEach((item) => {
+    articleLinks.forEach((link, i) => {
       const a = document.createElement('a');
-      const link = item.querySelector('link').textContent;
       a.href = link;
-      const title = item.querySelector('title');
-      const description = item.querySelector('description');
-      const textForLink = `  ${title.textContent || description.textContent.split('<')[0]}`;
-      a.textContent = textForLink;
-      const textForModal = description === null || description.textContent[0] === '<' ?
-        'This article does not have a description' : description.textContent.split('<')[0];
+      const title = articleTitles[i];
+      const description = articleDescriptions[i];
+      a.textContent = `  ${title}`;
       const li = document.createElement('li');
       const button = document.createElement('button');
       button.classList.add('btn', 'btn-outline-secondary', 'btn-sm');
@@ -88,8 +128,8 @@ export default () => {
       button.type = 'button';
       button.setAttribute('data-toggle', 'modal');
       button.setAttribute('data-target', '#modalDescription');
-      button.setAttribute('data-title', textForLink.trim());
-      button.setAttribute('data-description', textForModal);
+      button.setAttribute('data-title', title);
+      button.setAttribute('data-description', description);
       button.setAttribute('data-link', link);
       li.append(button);
       li.append(a);
@@ -118,18 +158,16 @@ export default () => {
     modalLink.attr('href', link);
   }).on('hide.bs.modal', () => {
     const buttons = $('li > button');
-    buttons.each((i, button) => {
-      $(button).css('box-shadow', 'none');
-    });
+    buttons.css('box-shadow', 'none');
   });
 
   inputFeed.addEventListener('input', () => {
     checkUrl(inputFeed.value);
     switch (state.checkUrlResult) {
-      case null:
+      case 'empty':
         inputFeed.className = 'form-control';
         break;
-      case false:
+      case 'invalid':
         inputFeed.classList.remove('is-valid');
         inputFeed.classList.add('is-invalid');
         break;
@@ -142,12 +180,15 @@ export default () => {
 
   inputForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    if (state.checkUrlResult === true) {
+    if (state.checkUrlResult === 'valid') {
       const feedUrl = inputFeed.value;
       launchDownloading();
       axios.get(`${proxy}${feedUrl}`)
         .then(response => manageLoadingState(response))
-        .then(data => parseRss(data, feedUrl))
+        .then(data => parseRss(data))
+        .then(parsedRss => extractRssElements(parsedRss))
+        .then(rssElements => extractRssData(rssElements))
+        .then(rssData => saveRss(rssData, feedUrl))
         .then(() => makeFeedList(feedUrl))
         .then(() => makeArticlesList(feedUrl))
         .catch((err) => {
@@ -155,7 +196,10 @@ export default () => {
           activateForm();
           mainAlert.classList.add('alert-danger');
           mainAlert.textContent = err;
-          timeout(4000);
+          setTimeout(() => {
+            mainAlert.className = '';
+            mainAlert.textContent = '';
+          }, 4000);
         });
     }
   });
